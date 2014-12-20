47f9ccbc7bbb28c74d3137804d7e28686da1de69
Carl Bäckström <genzorg@gmail.com>
2014-12-20 13:20:03 +0100

    Added first draft of installation scripts

---
+++ oracle_java.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get purge openjdk
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install oracle-java8-installer
---
+++ smartgit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-add-repository ppa:eugenesan/ppa
+sudo apt-get update
+sudo apt-get install smartgithg
