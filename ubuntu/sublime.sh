8309800eea8e6f146f905f4094fbcbcf3f4cc07c
Carl Backstrom <genzorg@gmail.com>
2015-01-05 02:15:14 +0100

    Added installation for google_chrome meld python sublime and a default with common dependencies.

---
+++ ubuntu/sublime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ../common/lib.sh
+
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get -y install sublime-text-installer
+ln -s /opt/sublime_text/sublime_text /usr/bin/sublime
