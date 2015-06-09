02bf3cad52a6983a0ab90eac8ca6c9576c4af425
Carl Backstrom <carl.backstrom@birdstep.com>
2015-06-09 20:03:55 +0200

    Added vagrant hostmanager plugiAdded vagrant hostmanager plugin

--- ubuntu/vagrant.sh
+++ ubuntu/vagrant.sh
@@ -7,6 +7,8 @@ tmp_dir=$(mktemp -d)
 cd $tmp_dir
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
 sudo dpkg -i vagrant_1.7.2_x86_64.deb
+vagrant plugin install vagrant-vbox-snapshot
+vagrant plugin install vagrant-hostmanager 
 cd $current_dir
 
 rm -rf $tmp_dir
