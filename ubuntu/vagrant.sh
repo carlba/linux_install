cdc67108b0c4ccd61cb57a4018cb96a8e177ef06
Carl Backstrom <carl.backstrom@birdstep.com>
2015-06-09 19:38:58 +0200

    dpkg installation of vagrant required sudo fixed

--- ubuntu/vagrant.sh
+++ ubuntu/vagrant.sh
@@ -6,7 +6,7 @@ tmp_dir=$(mktemp -d)
 
 cd $tmp_dir
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
-dpkg -i vagrant_1.7.2_x86_64.deb
+sudo dpkg -i vagrant_1.7.2_x86_64.deb
 cd $current_dir
 
 rm -rf $tmp_dir
