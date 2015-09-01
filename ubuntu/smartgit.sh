39aa378d56c840a14c9362a9b05cee8b04d7bacf
Carl Backstrom <carl.backstrom@birdstep.com>
2015-09-01 23:12:23 +0200

    Added smartgit script.

--- ubuntu/smartgit.sh
+++ ubuntu/smartgit.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-sudo apt-add-repository ppa:eugenesan/ppa
-sudo apt-get update
-sudo apt-get -y install smartgithg
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd $tmp_dir
+
+wget http://www.syntevo.com/downloads/smartgit/smartgit-6_5_9.deb
+sudo dpkg --install smartgit-6_5_9.deb
+
+[[ ! -f "/usr/bin/smartgit" ]] && sudo ln -s /usr/share/smartgit/bin/smartgit.sh /usr/bin/smartgit
+
+cd $current_dir
+
+rm -rf $tmp_dir
