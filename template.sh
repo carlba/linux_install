a8107269a15ff4f1ca8702fd30c36996e42325b6
Carl Backstrom <carl.backstrom@birdstep.com>
2015-09-05 11:14:06 +0200

    Removed extra spaces.

--- template.sh
+++ template.sh
@@ -6,8 +6,8 @@ tmp_dir=$(mktemp -d)
 
 cd $tmp_dir
 
-sudo add-apt-repository -f ppa:template                                                                                                                                                                                                                                                                   
-sudo apt-get update                                                                                                                                                                                                                                                                                                
+sudo add-apt-repository -f ppa:template
+sudo apt-get update
 sudo apt-get -y install template
 
 cd $current_dir
