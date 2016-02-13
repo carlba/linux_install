19ec259f352b6a6fcbc92f6b22848632ccad544c
Carl Backstrom <genzorg@gmail.com>
2016-02-13 13:14:07 +0100

    Oracle Java

--- ubuntu/oracle_java.sh
+++ ubuntu/oracle_java.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get purge openjdk
-sudo add-apt-repository -f ppa:webupd8team/java
-apt-get -y install oracle-java8-installer
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y install oracle-java8-installer
