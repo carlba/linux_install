2b03b603521b790702763d154a15c754c2233721
Carl Bäckström <genzorg@gmail.com>
2016-02-13 14:32:02 +0100

    Dropbox

---
+++ ubuntu/dropbox.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd $tmp_dir
+
+curl -LO https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
+sudo dpkg -i dropbox_2015.10.28_amd64.deb
+
+cd $current_dir
+
+rm -rf $tmp_dir
