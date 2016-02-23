12beac9aa0bd2b7083dcc6611d2bd3109694953a
Carl Backstrom <carl.backstrom@birdstep.com>
2016-02-23 16:06:45 +0100

    Added vim

---
+++ centos/vim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd $tmp_dir
+
+yum install -y vim-enhanced
+cp $current_dir/vim.tar.gz .
+cd ~
+tar -xzvf $tmp_dir/vim.tar.gz
+
+cd $current_dir
+
+rm -rf $tmp_dir
