28d6feb71cf27b84acb04745baf986883b36ea7b
Carl Backstrom <genzorg@gmail.com>
2016-03-08 11:58:31 +0100

    Ubuntu template

---
+++ ubuntu/template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd $tmp_dir
+
+sudo add-apt-repository -f ppa:template
+sudo apt-get update
+sudo apt-get -y install template
+
+cd $current_dir
+
+rm -rf $tmp_dir
