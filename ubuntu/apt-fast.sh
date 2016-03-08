e12756ef8e89e87f2fc4504c7e11245c4d722942
Carl Backstrom <genzorg@gmail.com>
2016-03-08 11:59:05 +0100

    apt-fast installation was made automatic

--- ubuntu/apt-fast.sh
+++ ubuntu/apt-fast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo add-apt-repository -f ppa:saiarcot895/myppa
+sudo add-apt-repository -y ppa:saiarcot895/myppa
 sudo apt-get update
-apt-get -y install apt-fast
+sudo apt-get -y install apt-fast
