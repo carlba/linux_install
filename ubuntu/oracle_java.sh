58035c5642c6d2d93347928e8534f056581789a2
Carl Backstrom <genzorg@gmail.com>
2016-03-08 11:57:41 +0100

    Oracle java install script is better at cleaning up openjdk stuff

--- ubuntu/oracle_java.sh
+++ ubuntu/oracle_java.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get purge openjdk
+sudo apt-get purge openjdk-*
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
