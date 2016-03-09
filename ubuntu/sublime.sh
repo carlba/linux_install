0d66e0a7117a4b7a995fad098e33448587642a78
Carl Backstrom <genzorg@gmail.com>
2016-03-09 01:58:38 +0100

    Sublime scripts symlinks Dropbox settings.

--- ubuntu/sublime.sh
+++ ubuntu/sublime.sh
@@ -6,3 +6,8 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get -y install sublime-text-installer
 sudo ln -s /opt/sublime_text/sublime_text /usr/bin/sublime
+
+mkdir -p ~/.config/sublime-text-3/Packages
+cd ~/.config/sublime-text-3/Packages/
+rm -r User
+ln -s ~/Dropbox/settings/Appdata/Sublime/User
