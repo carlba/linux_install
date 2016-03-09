ccc628bdbb1deb66410a52550172654efa796ee8
Carl Backstrom <genzorg@gmail.com>
2016-03-09 01:39:27 +0100

    Added xfce-theme-manager

---
+++ ubuntu/xfce-theme-manager.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd $tmp_dir
+
+sudo add-apt-repository -y ppa:rebuntu16/other-stuff 
+sudo apt-get update
+sudo apt-get install xfce-theme-manager
+cd $current_dir
+
+rm -rf $tmp_dir
