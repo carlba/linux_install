76016cf23ab02e2524d266a4d2321c1e3da6dc6a
Carl Backstrom <genzorg@gmail.com>
2016-03-09 01:38:46 +0100

    Creating sublime linux cli command is done elevated

--- ubuntu/sublime.sh
+++ ubuntu/sublime.sh
@@ -5,4 +5,4 @@ source ../common/lib.sh
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get -y install sublime-text-installer
-ln -s /opt/sublime_text/sublime_text /usr/bin/sublime
+sudo ln -s /opt/sublime_text/sublime_text /usr/bin/sublime
