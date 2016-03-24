3be135881fe95bd84014e28203bd586aa126bb86
Carl Backstrom <genzorg@gmail.com>
2016-03-24 14:33:00 +0100

    Added install script for Virtualboxwq

---
+++ ubuntu/virtualbox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd $tmp_dir
+
+# Dependencies
+sudo apt-get install dkms
+sudo apt-get update
+sudo apt-get install virtualbox
+
+cd $current_dir
+
+rm -rf $tmp_dir
