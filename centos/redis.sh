4ab020a7ab833b5c061e992b10d9a7eff1da095d
Carl Backstrom <carl.backstrom@birdstep.com>
2016-04-12 12:36:26 +0200

    Redis installation work unmanagedwq

--- centos/redis.sh
+++ centos/redis.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-yum --enablerepo=remi,remi-test install redis
+yum -y --enablerepo=remi,remi-test install redis
 
 chkconfig redis on
 
