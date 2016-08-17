af43d2e7d7f602ed1be7faa124c9e7880d989c8a
Carl Backstrom <genzorg@gmail.com>
2016-08-17 22:25:32 +0200

    Added dm.xmlsec.binding

--- ubuntu/google_chrome.sh
+++ ubuntu/google_chrome.sh
@@ -5,4 +5,6 @@ source ../common/lib.sh
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub |sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get -y update 
-sudo apt-get -y install google-chrome-stable 
+sudo apt-get -y install google-chrome-stable
+
+sudo sed -i 's#Exec=/opt/google/chrome/google-chrome %U#Exec=/opt/google/chrome/google-chrome --scroll-pixels=150 %U#g' /usr/share/applications/google-chrome.desktop
