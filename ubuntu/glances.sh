3c6ee7141ac8df79b585034a5f3ea42a85a5367d
Carl Backstrom <genzorg@gmail.com>
2016-10-02 10:02:02 +0200

    Made glances script silent

--- ubuntu/glances.sh
+++ ubuntu/glances.sh
@@ -7,9 +7,9 @@ tmp_dir=$(mktemp -d)
 cd $tmp_dir
 
 
-sudo apt-add-repository ppa:arnaud-hartmann/glances-stable
+sudo apt-add-repository -f ppa:arnaud-hartmann/glances-stable
 sudo apt-get update
-sudo apt-get install glances
+sudo apt-get -y install glances
 
 
 cd $current_dir
