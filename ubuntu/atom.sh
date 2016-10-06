91a34aa6d98c1da858a56cf14de7320513f9f1d0
Carl Backstrom <cbackstrom@smithmicro.com>
2016-10-06 23:14:14 +0200

    Added script for installing atom

--- ubuntu/atom.sh
+++ ubuntu/atom.sh
@@ -6,7 +6,7 @@ tmp_dir=$(mktemp -d)
 
 cd $tmp_dir
 
-sudo add-apt-repository -f ppa:webupd8team/atom
+sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get -y install atom
 
