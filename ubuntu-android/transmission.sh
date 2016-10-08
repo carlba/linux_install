fcdda3f63a6184df7ad846fc95582976b5843138
Carl Backstrom <genzorg@gmail.com>
2016-10-08 18:41:55 +0200

    Added ubuntu android install script for transmission

---
+++ ubuntu-android/transmission.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ../common/lib.sh
+current_dir=$PWD
+tmp_dir=$(mktemp -d)
+
+cd ${tmp_dir}
+
+sudo add-apt-repository -f ppa:transmissionbt/ppa
+sudo apt-get update
+sudo apt-get -y install transmission-daemon
+
+#Configure user running tranmission to have network access
+sudo usermod -a -G aid_inet debian-transmission
+
+mkdir -p $HOME/Downloads/transmission
+
+echo 'Please update /etc/transmission/settings.json with the correct download folder'
+
+
+cd ${current_dir}
+
+rm -rf ${tmp_dir}
