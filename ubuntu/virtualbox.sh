a905fb825a1f48a8af0d3b9c7a10f7d40bb9e9a3
Carl Bäckström <cada@servern.carlb>
2016-10-09 15:23:48 +0200

    Updated virtualbox install script

--- ubuntu/virtualbox.sh
+++ ubuntu/virtualbox.sh
@@ -7,9 +7,11 @@ tmp_dir=$(mktemp -d)
 cd ${tmp_dir}
 
 # Dependencies
-sudo apt-get install dkms
-sudo apt-get update
-sudo apt-get install virtualbox
+sudo apt remove virtualbox virtualbox-5.0 virtualbox-4.*
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo apt update
+sudo apt install virtualbox-5.1
 
 cd ${current_dir}
 
